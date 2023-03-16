0898f2d4531d4775c4467c88358858781a1f7c15
ZobairSoodi <zobair.soodi@gmail.com>
2023-03-16 16:29:00 +0100

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %lu byte(s)\n", sizeof(c));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(c));
+	printf("Size of a float: %lu byte(s)\n", sizeof(c));
+
+	return (0);
+}
