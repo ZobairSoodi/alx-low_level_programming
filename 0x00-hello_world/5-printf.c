fb99cee3974151f65838729a0a25b221ad38f86f
ZobairSoodi <zobair.soodi@gmail.com>
2023-03-16 16:18:16 +0100

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
