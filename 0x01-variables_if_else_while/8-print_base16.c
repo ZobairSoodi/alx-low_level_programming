335114e77ced3d602ed13ee9fe85a039354fc1c0
ZobairSoodi <zobair.soodi@gmail.com>
2023-03-17 19:13:54 +0100

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	char c;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'e'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
