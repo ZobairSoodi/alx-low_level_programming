cb4f2dcd9575cd7b291aa7bbf2e3f5cd2694f958
ZobairSoodi <zobair.soodi@gmail.com>
2023-03-17 19:01:20 +0100

    update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
