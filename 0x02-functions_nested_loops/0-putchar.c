a058aa5c9d1423e1b5877070f23bd941bb1ac732
ZobairSoodi <zobair.soodi@gmail.com>
2023-03-22 00:57:59 +0000

    update

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "holberton.h"
+
+/**
+ * main - Entry function
+ * Return: 0.
+ */
+int main(void)
+{
+	char word[8] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(word[i]);
+	_putchar('\n');
+
+	return (0);
+}
